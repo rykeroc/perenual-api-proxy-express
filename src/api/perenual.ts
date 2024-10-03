83d715beab3109adfa4e3c9936e8f8b75046f126
ryker <rykercooke@gmail.com>
2024-10-03 16:24:50 -0400

    Update perenual.ts: - Add /species/details endpoint - Fix issue with api key env variable

--- src/api/perenual.ts
+++ src/api/perenual.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import express from "express";
+import dotenv from "dotenv";
 
 export const perenualRouter = express.Router()
 
-const perenual_api_addr = "https://perenual.com/api"
+
+// Load Perenual API key from env
+dotenv.config();
 const api_key: string | undefined = process.env.PERENUAL_API_KEY
+
+const perenual_api_addr = "https://perenual.com/api"
 
 // Species list
 perenualRouter.get('/species-list', async (req, res) => {
@@ -23,9 +28,9 @@ perenualRouter.get('/species-list', async (req, res) => {
     const watering = req.query.watering || ""
     const sunlight = req.query.sunlight || ""
 
-    try {
-        const requestUrl = `${perenual_api_addr}/species-list?key=${api_key.toString()}&q=${q}&edible=${edible}&poisonous=${poisonous}&cycle=${cycle}&watering=${watering}&sunlight=${sunlight}&page=${page}&indoor=1`
+    const requestUrl = `${perenual_api_addr}/species-list?key=${api_key.toString()}&q=${q}&edible=${edible}&poisonous=${poisonous}&cycle=${cycle}&watering=${watering}&sunlight=${sunlight}&page=${page}&indoor=1`
 
+    try {
         // Forward request to Perenual API
         const response = await axios.get(
             requestUrl,
@@ -42,7 +47,29 @@ perenualRouter.get('/species-list', async (req, res) => {
 
 
 // Species Details
+perenualRouter.get('/species/details/:plantId', async (req, res) => {
+    // Check if API key
+    if (api_key === undefined) {
+        console.log('Api key is undefined. Check configuration.')
+        res.status(500).send('An unexpected error occurred.')
+        return
+    }
 
+    const plantId = req.params.plantId || ""
 
+    const requestUrl = `${perenual_api_addr}/species/details/${plantId}?key=${api_key.toString()}`
 
-module.exports = perenualRouter
+    try {
+        // Forward request to Perenual API
+        const response = await axios.get(
+            requestUrl,
+            {}
+        )
+        // Return response from Perenual APi
+        res.status(response.status).send(response.data)
+    } catch (err) {
+        // Log and return server error
+        console.error(err)
+        res.status(500).send('Error fetching data');
+    }
+})
