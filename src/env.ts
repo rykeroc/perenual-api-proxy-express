32d6080abaa404fc0ab883ee55d0c07d89a1445b
ryker <rykercooke@gmail.com>
2024-10-03 16:53:17 -0400

    Update package.json: Add winston package
    Add logging.ts: Configure and add global winston logger
    
    Update .gitignore: Add logs/
    
    Update env.ts: Add LOG_LEVEL

--- src/env.ts
+++ src/env.ts
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT: string = process.env.PORT || "8000";
 const PERENUAL_API_KEY: string | undefined = process.env.PERENUAL_API_KEY
+const LOG_LEVEL: string = process.env.LOG_LEVEL || "info";
 
 export {
     PORT,
     PERENUAL_API_KEY,
+    LOG_LEVEL,
 }
